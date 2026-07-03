d0687ba5db8b945645df686604a82f45d4601fc8
Ikram <ikrambtm444@gmail.com>
2026-07-04 01:30:32 +0500

    Use CloudinaryField

--- posts/views.py
+++ posts/views.py
@@ -10,15 +10,11 @@ from .Serilizer import PostSerializer
 def posts(request):
 
     if request.method == "GET":
-
-        posts = Post.objects.all().order_by("-created_at")
-
         serializer = PostSerializer(
-            posts,
+            Post.objects.all(),
             many=True,
             context={"request": request},
         )
-
         return Response(serializer.data)
 
     serializer = PostSerializer(
@@ -27,9 +23,7 @@ def posts(request):
     )
 
     if serializer.is_valid():
-
         serializer.save()
-
         return Response(
             serializer.data,
             status=status.HTTP_201_CREATED,
@@ -48,24 +42,19 @@ def post_detail(request, pk):
         post = Post.objects.get(pk=pk)
 
     except Post.DoesNotExist:
-
         return Response(
             {"error": "Post not found"},
             status=status.HTTP_404_NOT_FOUND,
         )
 
     if request.method == "GET":
-
         serializer = PostSerializer(
             post,
             context={"request": request},
         )
-
         return Response(serializer.data)
 
     if request.method == "PUT":
-
-        old_image = post.image
 
         serializer = PostSerializer(
             post,
@@ -75,20 +64,7 @@ def post_detail(request, pk):
         )
 
         if serializer.is_valid():
-
             serializer.save()
-
-            # Delete previous image if replaced
-            if (
-                old_image
-                and request.FILES.get("image")
-                and old_image.path != post.image.path
-            ):
-                import os
-
-                if os.path.isfile(old_image.path):
-                    os.remove(old_image.path)
-
             return Response(serializer.data)
 
         return Response(
@@ -98,4 +74,6 @@ def post_detail(request, pk):
 
     post.delete()
 
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    return Response(
+        status=status.HTTP_204_NO_CONTENT,
+    )
