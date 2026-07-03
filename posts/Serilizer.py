ddbbf703feef0c01859e32c8f76a9cd724ef5c44
Ikram <ikrambtm444@gmail.com>
2026-07-04 01:42:04 +0500

    updated

--- posts/Serilizer.py
+++ posts/Serilizer.py
@@ -3,7 +3,13 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = "__all__"
+
+    def get_image(self, obj):
+        if obj.image:
+            return obj.image.url
+        return None
